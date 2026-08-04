96d87e125294c56c1a947df59a240217159f9022
Arman Mahabri <mahabriarman@gmail.com>
2026-08-04 22:53:09 +0530

    todo:#12_create-user-mongoose-schema

---
+++ server/src/models/User.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+// Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  mobileNumber: {
+    type: String,
+    required: true,
+    trim: true
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
